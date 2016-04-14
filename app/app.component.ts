bacc7e33ee739214ff6c8d341f2c722ae629bccc
stevie wonder <stevie wonder>
2016-04-14 22:28:26 +0200

    added app.component

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from 'angular2/core'
+
+@Component({
+    selector: 'recipee-shopping-app',
+    templateUrl : 'app/app.component.html',
+    styleUrls : 'app/app.component.css',
+    
+})
